49b137123ef94ee6aad37008e7a7c47c51ab2365
Vlad Khitev <vkhitev@axon.dev>
2021-08-30 19:47:51 +0300

    fix: remove unused packages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserModule } from '@angular/platform-browser'
 import { AppComponent } from './app.component'
 import { FormBuilderComponent } from './form-builder/form-builder.component'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
-import { FormBuilder } from '@angular/forms'
 
 @NgModule({
   declarations: [AppComponent, FormBuilderComponent],
